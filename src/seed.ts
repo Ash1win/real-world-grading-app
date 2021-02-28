e2a12cb69edbac5c91fcc9f8f3dac7a720411310
Ashwin <naikashwin30@gmail.com>
2021-02-28 13:18:00 +0530

    complete

--- src/seed.ts
+++ src/seed.ts
@@ -4,7 +4,70 @@ import { add } from 'date-fns'
 const prisma = new PrismaClient()
 
 // A `main` function so that we can use async/await
-async function main() {}
+async function main() {
+
+  await prisma.user.deleteMany({})
+  await prisma.test.deleteMany({})
+  await prisma.course.deleteMany({})
+
+  // const grace = await prisma.user.create({
+  //   data: {
+  //     email: "grace@hey.com",
+  //     firstName: "Grace",
+  //     lastName: "Bell",
+  //     social: {
+  //       facebook: "gracebell",
+  //       twitter: "gracebell"
+  //     }
+  //   }
+  // })
+
+  // console.log(grace);
+
+  // const weekFromNow = add(new Date(), {days: 7})
+  // const twoWeeksFromNow = add(new Date(), {days: 14})
+  // const monthFromNow = add(new Date(), {days: 28})
+
+  // const course = await prisma.course.create({
+  //   data:{
+  //     name: "CRUD with prisma real world",
+  //     courseDetails: "An soft ontroduction to CRUD with prisma real world",
+  //     tests: {
+  //       create: [
+  //         {
+  //           date: weekFromNow,
+  //           name: "First test"
+  //         },
+  //         {
+  //           date: twoWeeksFromNow,
+  //           name: "Second test"
+  //         },
+  //         {
+  //           date: monthFromNow,
+  //           name: "Third test"
+  //         }
+  //       ]
+  //     },
+  //     members: {
+  //       create: {
+  //         role: "TEACHER",
+  //         user: {
+  //           connect: { email: grace.email }
+  //         }
+  //       }
+  //     }
+  //   },
+  //   include: {
+  //     tests: true,
+  //     members: {
+  //       include: {user: true}
+  //     }
+  //   }
+  // })
+
+  // console.log(course)
+
+}
 
 main()
   .catch((e: Error) => {
